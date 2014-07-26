9cff077dfd09b090dea6fa0315badd73d7ed414a
aliyarahman <aliya.rahman@yahoo.com>
2014-07-26 15:35:13 -0400

    Built out models

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# class User is built in to Django: has firsrtname, lastname, username, email, password
+# That User is referenced via foreign key to attach to the Provider profile
+
+class Provider(models.Model):
+	admin = models.ForeignKey(User) # This is what I just typed
+	admin firstname2 = models.CharField(max_length=45, default = "")
+	admin_lastname2 = models.CharField(max_length=45, default = "")		
+	name = models.CharField(max_length=140, unique = True)
+	logo = models.CharField(max_length=45)
+	URL = models.CharField(max_length=45)
+	
+	def __unicode__(self):
+		return self.name
+
+
+class Location(models.Model):
+	POC_firstname = models.CharField(max_length=45)
+	POC firstname2 = models.CharField(max_length=45, default = "")
+	POC_lastname = models.CharField(max_length=45)
+	POC_last2name = models.CharField(max_length=45, default="")
+	provider = models.ForeignKey(Provider)
+	address = models.CharField(max_length=140)
+	phone = models.CharField(max_length=20)
+	is_headquarters = models.BooleanField
+	hours_open = models.CharField(max_length=200)
+    resources_needed = models.ManyToManyField(Resource, related_name="resources_needed")
+    resources_available = models.ManyToManyField(Resource, related_name="resources_available")
+    
+
+class Resource(models.Model):
+	name = models.CharField(max_length=30, unique = True)
+	providers = models.ManyToManyField(Provider, default = [])
+
+	def __unicode__(self):
+		return self.name
+		
---
+++ app/views.py
@@ -0,0 +1,21 @@
+import urlparse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.core.urlresolvers import reverse
+from app.models import Provider, Resource 
+
+
+def index(request):
+	return render(request, "index.html")
+
+
+
+@login_required
+def profile(request):
+#	provider = get_object_or_404(City, pk=provider_id)
+	return render(request, "profile.html")
+#	return render(request, "profile.html", {'provider': provider })
