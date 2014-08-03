ef162f31e27d63c08f46465324d7dfea931662fc
aliyarahman <aliya.rahman@yahoo.com>
2014-08-03 14:33:24 -0400

    Added relationships to db_populate script

--- db_populate.py
+++ db_populate.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User
-from app.views import Resource, Provider, Location
+from app.views import *
 import csv
 
 # Add resources
-#for resource in ["food", "clothing", "language", "legal services", "transportation", "medical care", "education and enrollment", "religious services", "counseling", "housing"]:
-#	r = Resource(name=resource)
-#	r.save()
+for resource in ["food", "clothing", "language", "legal services", "transportation", "medical care", "education and enrollment", "religious services", "counseling", "housing"]:
+	r = Resource(name=resource)
+	r.save()
 
 # Load user
 aliya = User.objects.filter(username="aliya").first()
@@ -27,15 +27,23 @@ with open('providers.csv', 'rb') as csvfile:
 		if index >0:
 			p = Provider.objects.filter(name=row[0]).first()
 			l= Location(POC_firstname = "Aliya", POC_firstname2="", POC_lastname="", POC_lastname2="", provider = p, address = (str(row[1])+str(row[5])+str(row[6])+str(row[7])+str(row[8])), latitude=0.00, longitude=0.00, phone = row[10], is_headquarters=True, hours_open=row[11])
-			food = Resource.objects.filter(name="food")
-			clothing = Resource.objects.filter(name="clothing")
-			language = Resource.objects.filter(name="language")
-			legal = Resource.objects.filter(name="legal services")
-			transportation = Resource.objects.filter(name="transportation")
-			medical = Resource.objects.filter(name="medical care")
-			school = Resource.objects.filter(name="education and enrollment")
-			counseling = Resource.objects.filter(name="counseling")
-			housing = Resource.objects.filter(name="housing")
+			l.save()
+
+with open('providers.csv', 'rb') as csvfile:
+	providers = csv.reader(csvfile, delimiter=',', quotechar='"')
+	for index, row in enumerate(providers):
+		if index >0:
+			p = Provider.objects.filter(name=row[0]).first()
+			l= Location.objects.filter(provider=p).first()
+			food = Resource.objects.filter(name="food").first()
+			clothing = Resource.objects.filter(name="clothing").first()
+			language = Resource.objects.filter(name="language").first()
+			legal = Resource.objects.filter(name="legal services").first()
+			transportation = Resource.objects.filter(name="transportation").first()
+			medical = Resource.objects.filter(name="medical care").first()
+			school = Resource.objects.filter(name="education and enrollment").first()
+			counseling = Resource.objects.filter(name="counseling").first()
+			housing = Resource.objects.filter(name="housing").first()
 			if row[12] =='yes' or row[12]=='Yes':
 				l.resources_needed.add(food)
 				l.resources_available.add(food)
