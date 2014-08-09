83bf2eb0a9beb073e2990dabb820a665417610e3
Jan Rubio <jan.rubio@opower.com>
2014-08-09 14:51:44 -0400

    Adding back AbstractBaseUser import

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser
 
 from geopy.geocoders import GoogleV3
 
