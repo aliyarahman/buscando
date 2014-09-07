208e6d2a4da0b5c8abc19faba6b6729987915f23
aliyarahman <aliya.rahman@yahoo.com>
2014-09-07 12:42:22 -0400

    Updated settings for email

--- buscando/settings.py
+++ buscando/settings.py
@@ -110,7 +110,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'information@buscandomaryland.com'
-EMAIL_HOST_PASSWORD = 'password' #update this when we've got it
+EMAIL_HOST_PASSWORD = hidden_email_password
 
 
 
